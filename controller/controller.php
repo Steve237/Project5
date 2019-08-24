2f154b80499f5560c9340d9eb4f9e8cafbec8cd5
Essama23 <adouessono@yahoo.fr>
2019-08-24 22:48:41 +0200

    indentation of controller.php to blogposts section

--- controller/controller.php
+++ controller/controller.php
@@ -6,34 +6,35 @@ session_start();
 require_once ABSOLUTE_PATH."/model/NewsManager.php";
 require_once ABSOLUTE_PATH."/model/news.php";
 
-
-
-
 function sendMail() 
 {
 // $errors = [];
   $errors = array(); // on crée une vérif de champs
 if(!array_key_exists('name', $_POST) || $_POST['name'] == '') 
-{// on verifie l'existence du champ et d'un contenu
+  {// on verifie l'existence du champ et d'un contenu
   $errors ['name'] = "vous n'avez pas renseigné votre nom";
   }
+
 if(!array_key_exists('email', $_POST) || $_POST['email'] == '' || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) 
-{// on verifie existence de la clé
+  {// on verifie existence de la clé
   $errors ['mail'] = "vous n'avez pas renseigné votre email";
   }
-if(!array_key_exists('message', $_POST) || $_POST['message'] == '') {
+
+if(!array_key_exists('message', $_POST) || $_POST['message'] == '') 
+  {
   $errors ['message'] = "vous n'avez pas renseigné votre message";
   }
 
 //On check les infos transmises lors de la validation
-  if(!empty($errors)){ // si erreur on renvoie vers la page précédente
+if(!empty($errors))
+  { // si erreur on renvoie vers la page précédente
   $_SESSION['errors'] = $errors;//on stocke les erreurs
   $_SESSION['inputs'] = $_POST;
   header('Location: index.php#formContact');
   }
     
 else
- {
+  {
   $_SESSION['success'] = 1;
   $headers  = 'MIME-Version: 1.0' . "\r\n";
   $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
