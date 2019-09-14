f61799e240ebfda80661c87c17858ea16d6952fc
Essama23 <adouessono@yahoo.fr>
2019-09-14 09:55:45 +0200

    add function to recovery forget password

---
+++ index.php
@@ -0,0 +1,81 @@
+<?php 
+
+// Definition du chemin absolu
+define("ABSOLUTE_PATH", dirname(__FILE__));
+
+
+require ABSOLUTE_PATH.'/controller/frontend.php';
+require ABSOLUTE_PATH.'/controller/backend.php';
+
+
+if (isset($_GET['action']))
+{
+    
+if ($_GET['action'] == 'listposts') 
+{
+listPosts();
+
+}
+    
+elseif ($_GET['action'] == 'post') 
+{
+if (isset($_GET['id']) && $_GET['id'] > 0) 
+{
+post();
+
+} 
+    
+}
+    
+    
+elseif ($_GET['action'] == 'sendmail')
+{
+    sendMail();
+     
+}
+
+
+
+elseif ($_GET['action'] == 'inscription')
+{
+    
+
+ Inscription();
+    
+}
+
+
+elseif ($_GET['action'] == 'connection')
+{
+    
+
+ Connection();
+    
+}
+
+elseif ($_GET['action'] == 'recovery_pass')
+{
+    
+ Recovery();    
+    
+}
+
+
+elseif ($_GET['action'] == 'disconnected')
+{
+    
+ Disconnect();    
+    
+}
+
+
+}
+
+
+else
+{
+    
+homePage();
+    
+}
+    
